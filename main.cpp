24ad235dfd6cef1618229463edd55e81b1ac3f73
ANTDIL <antonio.dilernia@marconibari.edu.it>
2024-11-07 09:09:13 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int c, d, i, q, r;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    cout << "Inserisci un numero" << endl;
+    cin >> c;
+    i = 0;
+    for (d = 1; d <= c; d++) {
+        q = (int) ((double) c / d);
+        r = c % d;
+        if (r == 0) {
+            cout << d << "è divisore di " << c << endl;
+            i = i + 1;
+        }
+    }
+    if (i == 2) {
+        cout << c << "è un numero primo" << endl;
+    } else {
+        cout << c << "non è un numero primo" << endl;
+    }
+    return 0;
+}
